b42d0d248f24dd91bb7314a8ca993e9b8ece56f8
agiveygives <JustinCase25XXV@gmail.com>
2021-10-25 11:57:04 -0500

    ch 9. remember me functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
 
-    if user&.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       log_in user
+
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -15,7 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if(current_user)
     redirect_to root_url
   end
 end
